f814512432fd07f9527ee48f3453df0964711a41
新宿 <zhenhui.wzh@alibaba-inc.com>
2022-05-25 17:46:53 +0800

    prepare for dart pub publish. update pubspec.yaml:  power_image_ext: 2.5.3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PowerScrollView',
+      title: 'power_image',
       theme: ThemeData(
         // This is the theme of your application.
         //
